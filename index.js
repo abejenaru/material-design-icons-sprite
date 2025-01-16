03b8ef2f543bd720265c025d0fa5be99c2bcaba9
Andrei Bejenaru <andrei.bejenaru@ext.ec.europa.eu>
2025-01-16 10:14:29 +0100

    Bump dependencies; Use Node 22

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const SVGSpriter    = require('svg-sprite'),
-path                = require('path'),
-mkdirp              = require('mkdirp'),
-fs                  = require('fs');
+const SVGSpriter = require('svg-sprite');
+const path = require('node:path');
+const fs = require('node:fs');
 
-const ROOT_SVG_DIR = __dirname + '/node_modules/@material-design-icons/svg/';
+const ROOT_SVG_DIR = path.join(__dirname, 'node_modules/@material-design-icons/svg/');  
 
 const TYPES = [
     'filled',
@@ -15,9 +14,8 @@ const TYPES = [
     'two-tone'
 ];
 
-
-TYPES.forEach(function(type){
-    var config = {
+TYPES.forEach(async function(type) {  
+    const config = {
         "svg": {
             "xmlDeclaration": false,
             "doctypeDeclaration": false,
@@ -32,44 +30,52 @@ TYPES.forEach(function(type){
         }
     };
 
+    const spriter = new SVGSpriter(config);
+    const typePath = path.join(ROOT_SVG_DIR, type);
     
-    var spriter = new SVGSpriter(config);
+    try {
+        const files = fs.readdirSync(typePath);
 
-    var files = fs.readdirSync(ROOT_SVG_DIR + type);
+        // Register SVG files with the spriter
+        files.forEach(function(file) {
+            const filePath = path.join(typePath, file);
+            spriter.add(
+                path.resolve(filePath),
+                file,
+                fs.readFileSync(filePath, { encoding: 'utf-8' })
+            );
+        });
 
-    // Register SVG files with the spriter
-    files.forEach(function(file){
-        spriter.add(
-            path.resolve(ROOT_SVG_DIR + type + '/' + file),
-            file,
-            fs.readFileSync(path.resolve(ROOT_SVG_DIR + type + '/' + file), { encoding: 'utf-8' })
-        );
-    })
-
-    // Compile the sprites
-    spriter.compile(function(error, result) {
-        // Run through all configured output modes
-        for (var mode in result) {
-            // Run through all created resources and write them to disk
-            for (var type in result[mode]) {
-                mkdirp.sync(path.dirname(result[mode][type].path));
-                fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
-                cleanupSprite(result[mode][type].path);
+        // Compile the sprites
+        spriter.compile(function(error, result) {
+            if (error) {
+                console.error(`Error processing ${type}:`, error);
+                return;
             }
-        }
-    });
-
+            
+            // Run through all configured output modes
+            for (const mode in result) {
+                // Run through all created resources and write them to disk
+                for (const resourceType in result[mode]) {
+                    const resource = result[mode][resourceType];
+                    fs.mkdirSync(path.dirname(resource.path), { recursive: true });
+                    fs.writeFileSync(resource.path, resource.contents);
+                    cleanupSprite(resource.path);
+                    console.log(`Generated sprite: ${resource.path}`);
+                }
+            }
+        });
+    } catch (err) {
+        console.error(`Error processing directory ${typePath}:`, err);
+    }
 });
 
+function cleanupSprite(spriteFile) {
+    const fileContent = fs.readFileSync(spriteFile, 'utf8');
 
-
-function cleanupSprite (spriteFile)
-{
-    var fileContent = fs.readFileSync(spriteFile, 'utf8');
-
-    var result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
-                            .replaceAll(' xmlns:xlink="http://www.w3.org/1999/xlink"', '')
-                            .replace('<svg>', '<svg xmlns="http://www.w3.org/2000/svg">');
+    const result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
+        .replaceAll(' xmlns:xlink="http://www.w3.org/1999/xlink"', '')
+        .replace('<svg>', '<svg xmlns="http://www.w3.org/2000/svg">');
     
     fs.writeFileSync(spriteFile, result, 'utf8');
 }
